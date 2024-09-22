16fb8ef6c627a87fc99460cf3e6fc53c6b370a54
rcahuana987 <132010174+rcahuana987@users.noreply.github.com>
2024-09-22 14:13:49 -0600

    Making Progress with king

--- shared/src/main/java/chess/BishopMoves.java
+++ shared/src/main/java/chess/BishopMoves.java
@@ -1,1 +1,4 @@
 package chess;
+
+public class BishopMoves {
+}
--- shared/src/main/java/chess/RookMoves.java
+++ shared/src/main/java/chess/RookMoves.java
@@ -1,12 +1,4 @@
-// package chess;
+package chess;
 
-// import java.util.Collection;
-
-// public class RookMoves implements ChessPiece{
-//     private static validMoves[][] = YY
-//     public static final ROOK_MOVES() {
-//         int[][] validMoves = new Collection<moves>
-        
-//     }
-    
-// }
+public class RookMoves {
+}
