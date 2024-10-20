ffdd7392377596aa4ce9fa3721e9a5bd3a8ab859
rcahuana987 <132010174+rcahuana987@users.noreply.github.com>
2024-10-20 17:56:26 -0600

    Handling exceptions and creating services

---
+++ server/src/main/java/server/GameHandler.java
@@ -0,0 +1,14 @@
+package server;
+
+import com.google.gson.Gson;
+import dataaccess.SQLGameDAO;
+import model.GameData;
+
+public class GameHandler {
+    private SQLGameDAO = gameDAO;
+    private Gson gson = new Gson();
+    public GameHandler(SQLGameDAO gameDAO){{
+        this.gameService = gameService;
+    }
+    }
+}
