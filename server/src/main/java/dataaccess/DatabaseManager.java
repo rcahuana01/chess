98f52926bfb14fe918512e60419780c737d27dda
rcahuana987 <132010174+rcahuana987@users.noreply.github.com>
2024-10-26 21:25:47 -0600

    Completed DatabaseManager method executeUpdate

--- server/src/main/java/dataaccess/DatabaseManager.java
+++ server/src/main/java/dataaccess/DatabaseManager.java
@@ -1,5 +1,6 @@
 package dataaccess;
 
+import chess.ChessGame;
 import model.AuthData;
 
 import java.sql.Connection;
@@ -88,15 +89,25 @@ public class DatabaseManager {
         }
     }
 
-    static void executeUpdate(String statement, Object param){
-        try (var conn = DatabaseManager.getConnection()){
-            try(var ps = conn.prepareStatement(statement, Statement.RETURN_GENERATED_KEYS)){
-                for (var i =0; i < param.getClass(); i++){
+    static void executeUpdate(String statement, Object params) {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(statement, Statement.RETURN_GENERATED_KEYS)) {
+                for (var i = 0; i < params.getClass(); i++) {
                     var param = params[i];
-                    if (param instanceof String p) ps.setString(i+1,p);
-                    else if (param instanceof Integer p) ps.setInt(i+1,p);
-                    else if ()
+                    if (param instanceof String p) ps.setString(i + 1, p);
+                    else if (param instanceof Integer p) ps.setInt(i + 1, p);
+                    else if (param instanceof ChessGame p) ps.setInt(i + 1, p);
+                    else if (param == null) ps.setNull(i + 1, NULL);
                 }
+                ps.executeUpdate();
+                var rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    return rs.getInt(1);
+                }
+                return 0;
             }
+        } catch (SQLException e) {
+            throw new DataAccessException("Unable to update database" + e.getMessage());
         }
+    }
 }
