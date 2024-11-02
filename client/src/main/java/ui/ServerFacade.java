1d4845db0603f39aac88d4b4e6b627b99d38b48a
rcahuana987 <132010174+rcahuana987@users.noreply.github.com>
2024-11-01 20:43:04 -0600

    Completing ServerFacade.java file

--- client/src/main/java/ui/ServerFacade.java
+++ client/src/main/java/ui/ServerFacade.java
@@ -10,6 +10,7 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Map;
 
 public class ServerFacade {
     private final String serverURL;
@@ -26,12 +27,12 @@ public class ServerFacade {
         if (httpResponse.statusCode() == 200){
             return new Gson().fromJson(httpResponse.body(), AuthData.class);
         } else {
-            throw new Exception("Error: " + httpResponse.body(), AuthData.class);
+            throw new Exception();
         }
     }
 
     public AuthData login(UserData user) throws Exception{
-        HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create(serverUrl + "/session")).POST(HttpRequest.BodyPublishers.ofString(new Gson().toJson(user))).header("Content-Type", "application/json").build();
+        HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create(serverURL + "/session")).POST(HttpRequest.BodyPublishers.ofString(new Gson().toJson(user))).header("Content-Type", "application/json").build();
 
         HttpResponse<String> httpResponse = HttpClient.newHttpClient().send(httpRequest, HttpResponse.BodyHandlers.ofString());
 
@@ -43,14 +44,14 @@ public class ServerFacade {
     }
 
     public void logout(String authToken) throws Exception{
-        HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create(serverUrl + "/session")).DELETE().header("Authorization", authToken).build();
+        HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create(serverURL + "/session")).DELETE().header("Authorization", authToken).build();
 
         HttpResponse<String> httpResponse = HttpClient.newHttpClient().send(httpRequest, HttpResponse.BodyHandlers.ofString());
 
     }
 
     public ListGameData listGames(String authToken) throws Exception{
-        HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create(serverUrl + "/game")).GET().header("Authorization", authToken).build();
+        HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create(serverURL + "/game")).GET().header("Authorization", authToken).build();
 
         HttpResponse<String> httpResponse = HttpClient.newHttpClient().send(httpRequest, HttpResponse.BodyHandlers.ofString());
 
@@ -61,7 +62,35 @@ public class ServerFacade {
         }
     }
 
-    public GameData createGame(String authToken, GameData game) throws Exception{
-        HttpRequest httpRequest = HttpRequest.newBuilder().uri()
+    public GameData createGame(String authToken, GameData game) throws Exception {
+        HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create(serverURL + "/game")).POST(HttpRequest.BodyPublishers.ofString(new Gson().toJson(game))).header("Content-Type", "application/json").header("Authorization", authToken).build();
+
+        HttpResponse<String> httpResponse = HttpClient.newHttpClient().send(httpRequest, HttpResponse.BodyHandlers.ofString());
+
+        if (httpResponse.statusCode() == 200) {
+            return new Gson().fromJson(httpResponse.body(), GameData.class);
+        } else {
+            throw new Exception("Error: " + httpResponse.statusCode() + " " + httpResponse.body());
+        }
+    }
+
+    public void joinGame(String authToken, int gameId, String playerColor) throws Exception{
+        HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create(serverURL + "/game")).PUT(HttpRequest.BodyPublishers.ofString(new Gson().toJson(Map.of("playerColor", playerColor, "gameID", gameId)))).header("Content-Type", "application/json").header("Authorization", authToken).build();
+
+        HttpResponse<String> httpResponse = HttpClient.newHttpClient().send(httpRequest, HttpResponse.BodyHandlers.ofString());
+
+        if (httpResponse.statusCode() != 200){
+            throw new Exception("Error: " + httpResponse.statusCode() + " " + httpResponse.body());
+        }
+    }
+
+    public void clear() throws Exception{
+        HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create(serverURL + "/db")).DELETE().build();
+
+        HttpResponse<String> httpResponse = HttpClient.newHttpClient().send(httpRequest, HttpResponse.BodyHandlers.ofString());
+
+        if (httpResponse.statusCode() != 200){
+            throw new Exception("Error: " + httpResponse.statusCode() + " " + httpResponse.body());
+        }
     }
 }
