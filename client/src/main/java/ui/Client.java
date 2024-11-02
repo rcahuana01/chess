6ec8a36b26b9ac79d1ca0eadf5dd694de40832a9
rcahuana987 <132010174+rcahuana987@users.noreply.github.com>
2024-11-01 21:29:50 -0600

    Adding fields to Client.java

--- client/src/main/java/ui/Client.java
+++ client/src/main/java/ui/Client.java
@@ -1,15 +1,26 @@
 package ui;
 
+import chess.ChessBoard;
+import chess.ChessGame;
 import com.sun.nio.sctp.HandlerResult;
 import com.sun.nio.sctp.Notification;
 import com.sun.nio.sctp.NotificationHandler;
+import model.AuthData;
+import model.UserData;
 
 import java.util.Scanner;
 
 public class Client implements NotificationHandler {
 
+    private ClientState state = ClientState.PRE_LOGIN;
+    private ServerFacade serverFacade = new ServerFacade("http://localhost:8080");
 
     private Scanner state = new Scanner(System.in);
+    private AuthData authData = null;
+    private int currentGameId = -1;
+    private String currentPlayerColor = "";
+    private ChessBoard currentBoard = null;
+    private ChessGame currentGame = null;
 
     public void run() throws Exception{
         System.out.println("Welcome to Chess!");
@@ -20,6 +31,13 @@ public class Client implements NotificationHandler {
         try {
             System.out.println("Enter your username: ");
             String username = state.nextLine();
+            System.out.println("Enter your password: ");
+            String password = state.nextLine();
+            System.out.println("Enter your email: ");
+            String email = state.nextLine();
+
+            UserData userData = new UserData(username, password, email);
+            AuthData authData = serverFacade.reg
         }
     }
     @Override
