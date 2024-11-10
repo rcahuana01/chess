c78e005ca5ec6fa65c792f78318f9c27bd0a2e27
rcahuana987 <132010174+rcahuana987@users.noreply.github.com>
2024-11-10 11:19:24 -0700

    ChessBoard displaying

---
+++ client/src/main/java/ui/ChessBoard.java
@@ -0,0 +1,35 @@
+package ui;
+
+import chess.ChessGame;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.io.PrintStream;
+
+public class ChessBoard {
+    private ChessBoard gameBoard;
+    private ChessGame chessGame;
+    public ChessBoard(ChessBoard board, ChessGame game){
+        gameBoard = board;
+        chessGame = game;
+    }
+
+    public void drawBoard(PrintStream out, boolean reversed, ChessPosition piecePosition) {
+        ChessPiece[][] board = gameBoard.getBoard();
+        String[] labels = reversed ? new String[]{" h ", " g ", " f "}
+                : new String[]{};
+        printBorder(out, labels);
+
+        for (int row = 0; row < 8; row++){
+            int boardRow = reversed ? row : 7 - row;
+            out.print(" " + (reversed ? row + 1 : 8 - row) + " ");
+
+            String currentColor = (row % 2 == 0) ?
+        }
+    }
+
+    public void printBorder(PrintStream out, String[] labels) {
+        out.print("  ");
+        for (String label : labels)
+    }
+}
--- client/src/main/java/ui/Client.java
+++ client/src/main/java/ui/Client.java
@@ -10,12 +10,14 @@ import model.UserData;
 
 import java.util.Scanner;
 
+import static sun.java2d.windows.GDIBlitLoops.register;
+
 public class Client implements NotificationHandler {
 
     private ClientState state = ClientState.PRE_LOGIN;
-    private ServerFacade serverFacade = new ServerFacade("http://localhost:8080");
+    private final ServerFacade serverFacade = new ServerFacade("http://localhost:8080");
 
-    private Scanner state = new Scanner(System.in);
+    private Scanner scanner = new Scanner(System.in);
     private AuthData authData = null;
     private int currentGameId = -1;
     private String currentPlayerColor = "";
@@ -25,24 +27,49 @@ public class Client implements NotificationHandler {
     public void run() throws Exception{
         System.out.println("Welcome to Chess!");
         System.out.println();
+        do {
+            System.out.println("Available commands: ");
+
+            if(state == ClientState.PRE_LOGIN) {
+                displayPreloginCommands();
+
+                switch(scanner.nextLine()) {
+                    case "1":
+                    case "register":
+                        register();
+                        break;
+                    case "2":
+                        case ""
+                }
+            }
     }
 
-    public void register() throws Exception {
+    private void register() throws Exception {
         try {
             System.out.println("Enter your username: ");
-            String username = state.nextLine();
+            String username = scanner.nextLine();
             System.out.println("Enter your password: ");
-            String password = state.nextLine();
+            String password = scanner.nextLine();
             System.out.println("Enter your email: ");
-            String email = state.nextLine();
+            String email = scanner.nextLine();
 
             UserData userData = new UserData(username, password, email);
-            AuthData authData = serverFacade.reg
+            authData = serverFacade.register(userData);
+            System.out.println("You have registered your account and logged in successfully.");
+            state = ClientState.POST_LOGIN;
+        } catch (Exception e) {
+            System.out.println("Unable to register.");
+            System.out.println(e.getMessage());
         }
     }
+
+
     @Override
     public HandlerResult handleNotification(Notification notification, Object attachment) {
         return null;
     }
 }
+
+    private void displayPreloginCommands() {
+    }
 
