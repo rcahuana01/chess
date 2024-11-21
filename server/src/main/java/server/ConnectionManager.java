7b429f7670daff6af56f97989ae780a0629216bc
rcahuana987 <132010174+rcahuana987@users.noreply.github.com>
2024-11-20 23:16:36 -0700

    ConnetionManager to manage all active WebSocket connections

--- server/src/main/java/server/ConnectionManager.java
+++ server/src/main/java/server/ConnectionManager.java
@@ -1,4 +1,38 @@
 package server;
 
+import com.google.gson.Gson;
+import org.eclipse.jetty.websocket.api.Session;
+import websocket.messages.ServerMessage;
+
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class ConnectionManager {
+
+    public final ConcurrentHashMap<String, Connection> connections = new ConcurrentHashMap<>();
+    public void add(String visitorName, Session session, Integer gameId) {
+        var connection = new Connection(visitorName, session, gameId);
+        connections.put(visitorName, connection);
+    }
+
+    public void remove(String visitorName) {
+        connections.remove(visitorName);
+    }
+
+    public void broadcast(String excludeVisitor, ServerMessage notification, Integer gameId) throws Exception {
+        var removeList = new ArrayList<Connection>();
+        for (var c : connections.values()) {
+            if (c.session.isOpen()) {
+                if (!(c.visitorName.equals(excludeVisitor)) && c.gameId == gameId) {
+                    c.send(new Gson().toJson(notification));
+                }
+            } else {
+                removeList.add(c);
+            }
+        }
+
+        for (var c : removeList) {
+            connections.remove(c.visitorName);
+        }
+    }
 }
