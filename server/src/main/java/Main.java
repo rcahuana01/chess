473ae5f518a83a372bb7c79a1426a4dc602e1b37
rcahuana987 <132010174+rcahuana987@users.noreply.github.com>
2025-03-03 09:00:11 -0700

    Starting with the files

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,8 +1,13 @@
 import chess.*;
+import server.Server;
+import spark.*;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
+        Spark.port(8080);
+        Server server= new Server();
+        server.run(8080);
     }
 }
---
+++ server/src/main/java/server/Models.java
@@ -0,0 +1,7 @@
+package server;
+
+public class Models {
+    public class User {
+        
+    }
+}
